b149fb58847585d4c08d5163c18e5e5a8afc858b
Valeria Valle <valeria.valle@covacyt.com>
2021-01-31 03:32:28 -0600

    Stage Limit Fog

---
+++ Lost-GGJ/Assets/_LOST/Scripts/FakeFog.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FakeFog : MonoBehaviour
+{
+    #region VARIABLES
+
+    private Transform player;
+
+    [SerializeField]
+    private float increaseFogFactor = 0.1f;
+    [SerializeField]
+    private float detectDistance = 5f;
+    private float distance;
+
+    [SerializeField]
+    private Image fogImage;
+    #endregion
+
+    #region UNITY_METHODS
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(gameObject.transform.position, player.position);
+
+        if (distance <= detectDistance)
+        {
+            var tempColor = fogImage.color;
+            tempColor.a = increaseFogFactor / distance;
+            fogImage.color = tempColor;
+            Debug.Log("Increase Fog");
+        }
+        if (distance >= detectDistance)
+        {
+            var tempColor = fogImage.color;
+            tempColor.a = 0f;
+            fogImage.color = tempColor;
+            Debug.Log("Decrease Fog");
+        }
+    }
+    #endregion
+}
